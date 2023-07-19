16a6b1c7ad3bdbb8074e2b328bba6afe6b5a7b63
john.bellettati <john.bellettati@simchef.net>
2023-07-19 16:57:30 -0300

    resolving TS aliases problem on dev environment

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,5 @@
 import { setupContainer } from './containers/index'
 setupContainer()
-
 import app from './app'
-
-
 const PORT = 3333
 app.listen(PORT, () => console.log(`running on port ${PORT}`))
